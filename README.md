4dafc1eac15d8b090b66b44d4335951dd050583b
19h47 <jeremylevron@19h47.fr>
2019-05-29 13:18:55 +0200

    :tada: First commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# @19h47/tabs
+
+> Simple keycode helper
+
+It can be really helpful to have constants for keycodes.   
+That way when you look at your source 3 months later you won't have to remember what keycode `37` means :)
---
+++ src/index.js
@@ -0,0 +1,23 @@
+const TAB = 9;
+const SHIFT = 16;
+const ESCAPE = 27;
+const SPACE = 32;
+const END = 35;
+const HOME = 36;
+const LEFT = 37;
+const ARROWUP = 38;
+const RIGHT = 39;
+const ARROWDOWN = 40;
+
+export {
+	TAB,
+	SHIFT,
+	ESCAPE,
+	SPACE,
+	END,
+	HOME,
+	LEFT,
+	ARROWUP,
+	RIGHT,
+	ARROWDOWN,
+};
